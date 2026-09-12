9f11b4259961c276782b962997869f8f30760f6d
lithospark <156716874+Nothingbagelz@users.noreply.github.com>
2026-09-12 12:37:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ let inputtedNums;
 function printVal(value) {
   var x = value;
   document.getElementById("ansBox").innerHTML = value.concat(x);
+  console.log(x);
+  console.log(document.getElementById("ansBox").innerHTML);
 }
